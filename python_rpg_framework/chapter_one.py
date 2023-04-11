572b19e210319b5b7a6e6a2f0827912e9e843b10
hundredvisionsguy <winikkc@hsd.k12.or.us>
2023-04-11 16:21:07 -0700

    adding backstory and first step of game.

---
+++ python_rpg_framework/chapter_one.py
@@ -0,0 +1,19 @@
+backstory = ( 
+    "\nThe backstory of \"The Lost City of Maldonado\" is shrouded in mystery and" +
+    "\nlegend. According to local folklore, the city was once a thriving " +
+    "\nmetropolis, rich in gold and precious gems.",
+    "\nHowever, it is said that the city was cursed by the gods, and one day it " +
+    "\nvanished, leaving no trace behind.",
+    "\nFor centuries, the lost city was the subject of countless myths and legends" +
+    "\na team of archaeologists discovered evidence of its existence deep in the " +
+    "\njungles of South America. Excavations revealed a sprawling city, filled " +
+    "\ngrand temples, palaces, and intricate waterways.",
+    "\nHowever, as the team delved deeper into the ruins, they encountered strange " +
+    "\nand supernatural phenomena, leading some to believe that the curse of the " +
+    "\nwas still very much alive.",
+    "\nIntrigued by the discovery and the rumors surrounding the site, a group of " +
+    "\nadventurers has now ventured into the lost city in search of treasure, " +
+    "\nknowledge, and adventure.",
+    "But they soon discover that the ruins are teeming dangerous creatures, " +
+    "\nancient traps, and dark secrets, and that the curse of Maldonado is very " +
+    "\nreal and very deadly.\n")
---
+++ python_rpg_framework/game.py
@@ -0,0 +1,15 @@
+import character
+import chapter_one
+
+def exposite(text: tuple):
+    for paragraph in text:
+        # if not the last paragraph, add a ...
+        if paragraph != text[-1]:
+            description = paragraph
+            description += "\n\033[3m\033[95m\033[40mPress enter to continue...\033[0m"
+            input(description)
+        else:
+            input("\n" + paragraph)
+
+if __name__ == "__main__":
+    exposite(chapter_one.backstory)
